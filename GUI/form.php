f41f08cea470dc7915f4fcf4c883e296407b10de
Sptn2 <Sptn2@USUARIO-PC>
2015-11-29 11:14:14 -0500

    e

--- GUI/form.php
+++ GUI/form.php
@@ -1,36 +1,141 @@
+<?php
+session_start();
+//include '../logica/ControlPersona.php';
+include '../DB/ConexionDB.php';
+include '../logica/ControlFacultad.php';
+include '../logica/ControlPersona.php';
+include '../logica/ControlTipoCondicion_SE.php';
+include '../logica/ControlCondicion_SE.php';
+include '../logica/ControlSolicitud.php';
+include '../logica/ControlConvocatoria.php';
+include '../logica/ControlEstudiante.php';
+include '../logica/ControlCondicionxSolicitud.php';
+?>
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<?php 
-session_start();
-include '../logica/ControlCondicion_SE.php';
-include '../logica/ControlTipoCondicion_SE.php';
-include '../DB/ConexionDB.php';
-
-?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Formulario</title>
+
+        <link href="../public/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="../public/css/index_style.css">
+        <link rel="stylesheet"  href="../public/css/style.css"/>
+        <script src="../public/js/bootstrap.min.js"></script> 
     </head>
     <body>
-        
         <?php
-                                
+        include 'headerlog.php';
+        ?>
+
+        <?php
+        include 'menuLateral.php';
+        ?>
+
+        <div class="container">
+            <div class="col-sm-8">
+
+                <form id="form" method = "POST" > 
+                    <div class="col-sm-8">
+
+                        <div><br><br><br>
+
+                            <div>
+
+                                <?php
+                                $cFacultad = new ControlFacultad();
+                                $cConvocatoria = new ControlConvocatoria();
                                 $conn = new ConexionDB($_SESSION['usuario_login'], $_SESSION['password_login']);
                                 if ($conn->conectarDB()) {
                                     $sesion = $conn->getConn();
                                     $_SESSION['sesion_logueado'] = $sesion;
                                     //echo $sesion;
-                                    
+                                    $cPersona = new ControlPersona();
+                                    $persona = new Persona();
+                                    $persona = $cPersona->buscarPersonaxUsuario($_SESSION['usuario_login']);
+
+                                    $cSolictud = new ControlSolicitud();
+                                    $persona_documento = $persona->getDocumento_persona();
+
+                                    $cEstudiante = new ControlEstudiante();
+                                    $estudiante = $cEstudiante->buscarEstudiantexDocumento($persona_documento);
+                                    $estudiante_codigo = $estudiante->getCodigo_estudiante();
+
+                                    $cCondicionxSolicitud = new ControlCondicionxSolicitud();
+
+                                    //echo $persona->getNombre_persona()." ".$persona->getApellido_persona(); //->getNombre_persona()." ".$persona->getTipo_persona();
+                                    //echo $_SESSION['usuario_logueado'];
                                 }
                                 ?>
-        <h1>Andrey :D</h1>
-        <form>
-            <div class="container">
+                                <div class="focus" style="width: 1075px">
+                                    <center><h3 class="form-signin-heading">Informacion Personal</h3></center>
+                                    <div class="col-sm-6"><center><?php
+                                echo '<h3 class="form-signin-heading"> Nombre: ' . $persona->getNombre_persona() . ' ' . $persona->getApellido_persona() . '</h3>';
+                                ?></center></div>
+
+                                                                      <div class="col-sm-6"><center><?php
+                                            echo '<h3 class="form-signin-heading"> Documento: ' . $persona->getDocumento_persona() . '</h3>';
+                                ?></center></div>
+                                    <div class="col-sm-6"><center><?php
+                                            echo '<h3 class="form-signin-heading"> Codigo: ' . $estudiante_codigo . '</h3>';
+                                ?></center></div>
+                                    <div class="col-sm-6"><center><?php
+                                            echo '<h3 class="form-signin-heading"> Sexo: ' . $persona->getGenero_persona() . '</h3>';
+                                ?></center></div>
+
+                                </div>
+                                <hr>
+                            </div>
+
+
+
+                            <?php
+                            if (isset($_POST['submit'])) {
+                                //echo 'ALGO ESTA PASANDO???????????????????????????????????';
+                                //cho seleccion[];
+
+                                $archivo_temp = $_POST['file'];
+//                                $archivo = $_FILES["file"]['name'];
+//                                $destino = "C:/Users/ANDREY/Documents/".$archivo;
+//                                move_uploaded_file($_FILES['file']['tmp_name'], $destino);
+                              
+                               //eciho $archivo_temp;
+                              //$estudiante=new Estudiante();
+                                if($estudiante->getActivo_estudiante()=='SI'){
+                                if ($estudiante->getAval_solicitud()=='NO'){
+                                $cSolictud->CrearSolicitud($estudiante_codigo, $_POST['id_convocatoriasol']);//, $archivo_temp);
+                                $idsolicitud=$cSolictud->buscarIdSolicitud($estudiante_codigo, $_POST['id_convocatoriasol']);
+                                $cEstudiante->banderaActivo_solicitud($estudiante);
+                                foreach ($_POST['seleccion'] as $sel) {
+                                    echo $sel;
+                                    echo "--->(".$cCondicionxSolicitud->verTipoCondicionxSolicitud($sel)." )  AQUIIIIIIIIIIIIIIIIIIIIII";
+                                    echo "entroo";
+                                    //$id_condicion,$id_solicitud,$descripcion,$soportes_solicitud,$validado
+                                   $cCondicionxSolicitud->crearCondicionxSolicitud($sel, $idsolicitud, "na",$_POST['file'][$cCondicionxSolicitud->verTipoCondicionxSolicitud($sel)-1],'NO');
+                                }
+                                }else{                                
+                                echo '<script type="text/javascript">alert("El Estudiante Ya tiene una solicitud enviada");</script>';                                    
+                                    
+                                }
+                                }
+                                else{
+                                    echo '<script type="text/javascript">alert("El Estudiante no esta activo");</script>';                                    
+                                                                       
+                                }
+                            }
+                            ?>
+
+
+                            <div>
+                                <br>
+
+                                <div>
+                                    <div class="container">
                                         
                                             <?php
                                             //$tipo = new Tipo_Condicion_SE();
@@ -39,9 +144,9 @@ include '../DB/ConexionDB.php';
                                             $cond = new ControlCondicion_SE();
                                             foreach ($tipo_cond->verTipos_Condiciones() as $tipo) {
                                                 echo '<div class="containerl2">';
-                                                echo '';
+                                                echo '<div class="col-sm-12">';
                                                 echo '<div class="container" style="width: 1075px">';
-                                                echo '<center><h3 class="form-signin-heading">' . $tipo->getNombre_tipo_condicion() . '</h3></center>';
+                                                echo '<center><h3 class="form-signin-heading">' . $tipo->getNombre_tipo_condicion() . '</h3></center></div>';
                                                 //$condi=new Condicion_SE();
                                                 foreach ($cond->verCondiciones_SE($tipo->getId_tipo_condicion()) as $condi) {
                                                     echo '<div class="col-sm-6">';
@@ -54,17 +159,82 @@ include '../DB/ConexionDB.php';
                                                     echo '</div>';
                                                     //$contador+=1;
                                                 }
-                                               
-                                            echo '<br> <input name="file" type="file"  /></center>';
+                                                    echo '<div class="col-sm-12"><center>';
+                                                    echo '<br> <input name="file[]" value="'.$tipo->getId_tipo_condicion().'" type="file"  /></center></div>';
+                                                
                                                 echo '</div>';
-
                                                 echo '<hr>';
                                                 echo '</div>';
                                             }
                                             ?>
                                             <div class="col-sm-6">
-            
-        </form>
-        
-    </body>
-</html>
+                                            </div>                     
+
+                                        
+                                    </div>
+
+                                    <div class="container" >
+                                        <br>
+                                        <h3 class="form-signin-heading">Seleccione La convocatoria Para enviar Solicitud</h3>
+                                        <select id="subject" name="id_convocatoriasol" class="form-control" required="required">
+                                            <?php
+                                            //->verFacultades();
+                                            //$facul=new Facultad();
+                                            $convo = new Convocatoria();
+
+                                            foreach ($cConvocatoria->verConvocatoriasActivas() as $convo) {
+                                                echo $convo->getCupos() . '  ' . $convo->getPeriodo();
+                                                $facul = $cFacultad->buscarFacultad($convo->getId_facultad());
+                                                echo '<option value="' . $convo->getId_convocatoria() . '">Convocatoria  ' . $facul->getNombre_facultad() . '  cupos:  ' . $convo->getCupos() .
+                                                '  inicio:  ' . $convo->getFecha_inicio() . '  fin:  ' . $convo->getFecha_fin() . '</option>';
+                                            }
+
+                                            /* foreach ($cFacultad->verFacultades() as $fa) {
+                                              echo '<option value="' . $fa->getId_facultad() . '">' . $fa->getNombre_facultad() . '</option>';
+                                              } */
+                                            ?> 
+                                        </select>
+                                        <br>
+
+
+
+                                        
+
+                                    </div>
+
+                                    <div class="container" >
+
+
+
+                                        <br>
+                                        <div class="col-sm-12"> </div>
+                                        <div class="col-sm-12" style="padding-top: 10%"> 
+                                            <button class= "btn btn-primary btn-block" type="submit" name="submit">Enviar</button>  
+                                        </div>
+                                        <div class="col-sm-12" style="padding-left:10%"> </div>
+                                    </div>
+
+
+                                </div>                         
+                            </div> 
+                            </form>
+                        </div>
+                    </div>
+                    <br><br>
+
+                    <div id="footer">
+                        <div>
+                            <br></br>
+                            <br></br>
+
+                            <center><p>&#169; 2015 Bases de Datos 2.</p></center>
+
+
+                        </div>
+                    </div>
+                    </body>
+
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+                    <!-- Include all compiled plugins (below), or include individual files as needed -->
+                    <script src="public/js/bootstrap.min.js"></script> 
+                    </html>
